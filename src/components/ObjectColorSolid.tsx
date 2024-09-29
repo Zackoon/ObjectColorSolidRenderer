5186943a890fa0f54c1a040b8188a47a1255bed5
pritzza <jonathanzakharov@gmail.com>
2024-09-29 12:36:22 -0700

    cube

--- src/components/ObjectColorSolid.tsx
+++ src/components/ObjectColorSolid.tsx
@@ -1,24 +1,25 @@
-import { Skeleton, Stack } from "@mantine/core";
+import { Stack } from "@mantine/core";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
-interface ObjectColorSolidProps  {
-    height: number
+interface ObjectColorSolidProps {
+  height: number;
 }
 
 export default function ObjectColorSolid({ height }: ObjectColorSolidProps) {
-    return (
-        <Stack>
-            {/* <Canvas>
-               <mesh>
-                    <boxGeometry args={[5, 5, 5]}/>
-                    <meshStandardMaterial />
-                </mesh> 
-            </Canvas> */}
-            
-            <Skeleton height={height}>
-
-            </Skeleton>
-        </Stack>
-
-    )
+  return (
+    <Stack>
+      <div style={{ height: `${height}px` }}>
+        <Canvas camera={{ position: [0, 0, 10] }}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+          <mesh>
+            <boxGeometry args={[3, 3, 3]} />
+            <meshStandardMaterial color="royalblue" />
+          </mesh>
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </Stack>
+  );
 }
