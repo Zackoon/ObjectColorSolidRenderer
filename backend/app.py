9a3bc4afd0801dcc49dfffccdc817b61d2700e43
Zackoon <zackaryoon134@berkeley.edu>
2024-10-01 14:08:24 -0700

    Add example api call with parameters

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/ocs/helloworld') # ‘https://www.google.com/‘
+@app.route('/ocs/helloworld', methods=['GET']) # ‘https://www.google.com/‘
 def hello_world():
 	return jsonify(message="Hello, world!")
+
+@app.route('/ocs/range', methods=['GET']) # ‘https://www.google.com/‘
+def get_range():
+	start = request.args.get('start')
+	end = request.args.get('end')
+	
+	return jsonify(message=list(range(int(start), int(end))))
 
 app.run(port=5000)
